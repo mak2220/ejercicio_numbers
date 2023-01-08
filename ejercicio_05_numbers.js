c7bcb01de14eba04a2393a82e387b20417d4a3d7
mak2220 <113451174+mak2220@users.noreply.github.com>
2023-01-08 12:25:44 -0300

    Add files via upload

---
+++ ejercicio_05_numbers.js
@@ -0,0 +1,13 @@
+const altura = 183;
+
+const alturaFloat = 1.83;
+
+const peso = 90.5;
+
+const altura2 = Math.ceil(alturaFloat);
+
+const peso2 = Math.floor(peso);
+
+const igualdad = Number.MAX_VALUE+1 === Number.MAX_VALUE;
+
+
